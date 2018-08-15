9198e3c8d32bdfd8a74068b57a23e30d95fa6da9
Tania Miahkoholova <tanya@icemint.co>
2018-08-15 16:36:56 +0300

    raised max-results for 40

--- src/actions/bookActions.js
+++ src/actions/bookActions.js
@@ -4,7 +4,7 @@ export const getBooks = (value) => {
     return (dispatch) => {
         request
             .get('https://www.googleapis.com/books/v1/volumes')
-            .query({ q: value }) //'q' as the origin url looks like:'https://www.googleapis.com/books/v1/volumes?q=sea'
+            .query({ q: value, maxResults: 40 }) //'q' as the origin url looks like:'https://www.googleapis.com/books/v1/volumes?q=sea'
             .end((err, res) => {
                 switch (res.statusCode) {
                     case 200:
